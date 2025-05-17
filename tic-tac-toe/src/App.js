16d3f2ed9f83244698ab161a32960399f61c0218
Lucky Ali <luckyali786ashu@gmail.com>
2025-05-17 22:48:58 +0530

    Add files via upload

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './Tic-Tac-Toe/TicTacToe';
+
+function App() {
+  return (
+    <>
+      <TicTacToe></TicTacToe>
+    
+    </>
+  );
+}
+
+export default App;
